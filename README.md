d3ab9c86fb78fb7f07ba943815aab76f10110886
Wayne <wuw@gmx.com>
2022-05-27 23:08:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Webstack网址导航 美化版
+===
+
+### Demo: [3301.ml/](https://3301.ml/)
+
+喜欢给个Star~
+
+
+### Webstack网址导航
+
+### 原作者项目
+
+[https://github.com/WebStackPage/WebStackPage.github.io](https://github.com/WebStackPage/WebStackPage.github.io)
+
+### Demo: [➡️ www.webstack.cc](https://webstack.cc)
+
+### 参考魔改项目
+
+[https://github.com/arleycn/arleybookmarks](https://github.com/arleycn/arleybookmarks)
+
+### Demo: [➡️ bm.arley.cn/](https://bm.arley.cn/)
---
+++ js/getBrowser.js
@@ -0,0 +1,28 @@
+        //判断用户使用的设备
+        var deviceVal  = browserRedirect();
+        function browserRedirect() {
+        var sUserAgent = navigator.userAgent.toLowerCase();
+        var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+        var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+        var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+        var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+        var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+        var bIsAndroid = sUserAgent.match(/android/i) == "android";
+        var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+        var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+        if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+            console.log(" %c Navigation By Wayne %c https://3301.cf/ ", "color: #d2136b; background: #c7c4ea; border-radius: 5px; padding:5px;", "padding:5px;"),
+            document.write('<link rel="stylesheet" href="css/bootstrapmo.css">');
+            document.write('<link rel="stylesheet" href="css/xenon-coremo.css">');
+            document.write('<link rel="stylesheet" href="css/xenon-componentsmo.css">');
+            document.write('<div id="bg" style="background-image: url(images/bgmobile.jpg)"></div>');
+            return 'phone';
+        } else {
+            console.log(" %c Navigation By Wayne %c https://3301.cf/ ", "color: #ffffff; background: #6ea9e5;  border-radius: 5px; padding:5px;", "padding:5px;"),
+            document.write('<link rel="stylesheet" href="css/bootstrap.css">');
+            document.write('<link rel="stylesheet" href="css/xenon-core.css">');
+            document.write('<link rel="stylesheet" href="css/xenon-components.css">');
+            document.write('<div id="bg" style="background-image: url(images/bg.jpg)"></div>');
+            return 'pc';
+            }
+        }
