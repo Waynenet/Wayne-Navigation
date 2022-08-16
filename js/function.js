113f96ae8203fff4dd39893ea6e03cc2dd3db374
Wayne <waynenet8@gmail.com>
2022-08-16 21:40:55 +0800

    2.3.8 修复天气显示问题，新增网站

--- js/function.js
+++ js/function.js
@@ -189,8 +189,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：2.3.7
-更新日期：2022-08-15
+版 本 号：2.3.8
+更新日期：2022-08-16
 
 WayneのNavigation: https://3301.ml/
 Github:  https://github.com/Waynenet/Wayne-Navigation
