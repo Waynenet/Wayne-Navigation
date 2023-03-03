940715c475f72bff9537d47b5513316173f4e6ff
Wayne <waynenet8@gmail.com>
2023-03-03 20:50:49 +0800

    网站删减调整

--- js/function.js
+++ js/function.js
@@ -189,8 +189,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：2.4.2
-更新日期：2023-03-02
+版 本 号：2.4.3
+更新日期：2023-03-03
 
 WayneのNavigation: https://3301.ml/
 Github:  https://github.com/Waynenet/Wayne-Navigation
