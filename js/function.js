da74dddff488bf97aac3908e193450f2c51d9b0f
Wayne <waynenet8@gmail.com>
2023-10-02 11:43:47 +0800

    2.6.3网站删减，部分优化

--- js/function.js
+++ js/function.js
@@ -189,8 +189,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：2.6.2
-更新日期：2023-05-21
+版 本 号：2.6.3
+更新日期：2023-10-02
 
 WayneのNavigation: https://nav.kong.pub/
 Github:  https://github.com/Waynenet/Wayne-Navigation
