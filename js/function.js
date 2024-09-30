c69b5644aabd37b82e7733611b8d9c7ecda031c8
Wayne <waynenet8@gmail.com>
2024-09-30 15:24:45 +0800

    3.0.1 图片修复

--- js/function.js
+++ js/function.js
@@ -1,3 +1,41 @@
+$(document).ready(function() {
+    //img lazy loaded
+    const observer = lozad();
+    observer.observe();
+    return $(document).on("click", ".has-sub", function () {
+        var e = $(this);
+        $(this).hasClass("expanded") ? $(".has-sub ul").each(function (s, i) {
+            var t = $(this);
+            e.find("ul")[0] != i && setTimeout(function () {
+                t.attr("style", "")
+            }, 300)
+        }) : setTimeout(function () {
+            e.find("ul").attr("style", "")
+        }, 300)
+    }), $(".user-info-menu .d-sm-none .d-md-block").click(function () {
+        $(".sidebar-menu").hasClass("collapsed") ? $(".has-sub.expanded > ul").attr("style",
+            "") : $(".has-sub.expanded > ul").show()
+    }), $("#main-menu li ul li").click(function () {
+        $(this).siblings("li").removeClass("active"), $(this).addClass("active")
+    }), $("a.smooth").click(function (s) {
+        s.preventDefault(), public_vars.$mainMenu.add(public_vars.$sidebarProfile).toggleClass(
+            "mobile-is-visible"), ps_destroy(), $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top - 30
+        }, {
+            duration: 500,
+            easing: "swing"
+        })
+    }), !1
+});
+var href = "",
+    pos = 0;
+$("a.smooth").click(function (s) {
+    $("#main-menu li").each(function () {
+            $(this).removeClass("active")
+        }), $(this).parent("li").addClass("active"), s.preventDefault(), href = $(this).attr("href"),
+        pos = $(href).position().top - 30
+})
+
 function imgerrorfun(){ 
     var img=event.srcElement; 
     img.src="images/browser.svg"; //默认图片
@@ -150,7 +188,7 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：3.0.0
+版 本 号：3.0.1
 更新日期：2024-09-30
 
 WayneのNavigation: https://nav.kong.pub/
