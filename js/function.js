1bb6b717a561d4cfde24c91f7a9789fa0964a880
Wayne <waynenet8@gmail.com>
2024-10-01 21:57:52 +0800

    3.0.2 提示框修复

--- js/function.js
+++ js/function.js
@@ -188,8 +188,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：3.0.1
-更新日期：2024-09-30
+版 本 号：3.0.2
+更新日期：2024-10-01
 
 WayneのNavigation: https://nav.kong.pub/
 Github:  https://github.com/Waynenet/Wayne-Navigation
