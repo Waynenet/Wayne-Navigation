691ea5bcb22be68a3790fce86bcbec8aff65422b
Wayne <waynenet8@gmail.com>
2024-10-17 15:36:36 +0800

    4.0.1 代码精简，其他优化

--- js/function.js
+++ js/function.js
@@ -47,6 +47,27 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+//搜索框
+eval(function (e, t, a, c, i, n) {
+    if (i = function (e) {
+        return (e < t ? "" : i(parseInt(e / t))) + (35 < (e %= t) ? String.fromCharCode(e + 29) : e.toString(
+            36))
+        }, !"".replace(/^/, String)) {
+        for (; a--;) n[i(a)] = c[a] || i(a);
+        c = [function (e) {
+        return n[e]
+        }], i = function () {
+        return "\\w+"
+        }, a = 1
+    }
+    for (; a--;) c[a] && (e = e.replace(new RegExp("\\b" + i(a) + "\\b", "g"), c[a]));
+    return e
+    }('!2(){2 g(){h(),i(),j(),k()}2 h(){d.9=s()}2 i(){z a=4.8(\'A[B="7"][5="\'+p()+\'"]\');a&&(a.9=!0,l(a))}2 j(){v(u())}2 k(){w(t())}2 l(a){P(z b=0;b<e.O;b++)e[b].I.1c("s-M");a.F.F.F.I.V("s-M")}2 m(a,b){E.H.S("L"+a,b)}2 n(a){6 E.H.Y("L"+a)}2 o(a){f=a.3,v(u()),w(a.3.5),m("7",a.3.5),c.K(),l(a.3)}2 p(){z b=n("7");6 b||a[0].5}2 q(a){m("J",a.3.9?1:-1),x(a.3.9)}2 r(a){6 a.11(),""==c.5?(c.K(),!1):(w(t()+c.5),x(s()),s()?E.U(b.G,+T X):13.Z=b.G,10 0)}2 s(){z a=n("J");6 a?1==a:!0}2 t(){6 4.8(\'A[B="7"]:9\').5}2 u(){6 4.8(\'A[B="7"]:9\').W("14-N")}2 v(a){c.1e("N",a)}2 w(a){b.G=a}2 x(a){a?b.3="1a":b.16("3")}z y,a=4.R(\'A[B="7"]\'),b=4.8("#18-C-19"),c=4.8("#C-12"),d=4.8("#17-C-15"),e=4.R(".C-1b"),f=a[0];P(g(),y=0;y<a.O;y++)a[y].D("Q",o);d.D("Q",q),b.D("1d",r)}();',
+    62, 77,
+    "||function|target|document|value|return|type|querySelector|checked||||||||||||||||||||||||||var|input|name|search|addEventListener|window|parentNode|action|localStorage|classList|newWindow|focus|superSearch|current|placeholder|length|for|change|querySelectorAll|setItem|new|open|add|getAttribute|Date|getItem|href|void|preventDefault|text|location|data|blank|removeAttribute|set|super|fm|_blank|group|remove|submit|setAttribute"
+    .split("|"), 0, {})
+);
+
 //回到顶部
 window.onscroll = function () {
     if (document.documentElement.scrollTop + document.body.scrollTop > 100) {
@@ -245,8 +266,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：4.0.0
-更新日期：2024-10-15
+版 本 号：4.0.1
+更新日期：2024-10-17
 
 WayneのNavigation: https://
 Github:  https://github.com/Waynenet/Wayne-Navigation
