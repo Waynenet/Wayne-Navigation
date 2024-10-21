04fdf73435976cfbd69e8cdb4b8f711af6e08956
Wayne <waynenet8@gmail.com>
2024-10-21 13:19:18 +0800

    4.1.0 代码优化，暗黑模式下搜索栏UI修复，其他优化

--- js/function.js
+++ js/function.js
@@ -190,6 +190,264 @@ function stars() {
 stars()
 
 //侧边栏菜单键
+var public_vars = public_vars || {};
+
+;(function($, window, undefined){
+
+	"use strict";
+
+	$(document).ready(function()
+	{
+		// Main Vars
+		public_vars.$body                 = $("body");
+		public_vars.$pageContainer        = public_vars.$body.find(".page-container");
+		public_vars.$sidebarMenu          = public_vars.$pageContainer.find('.sidebar-menu');
+		public_vars.$sidebarProfile       = public_vars.$sidebarMenu.find('.sidebar-user-info');
+		public_vars.$mainMenu             = public_vars.$sidebarMenu.find('.main-menu');
+
+		public_vars.$mainContent          = public_vars.$pageContainer.find('.main-content');
+		public_vars.$mainFooter           = public_vars.$body.find('footer.main-footer');
+
+		public_vars.$userInfoMenu         = public_vars.$body.find('nav.navbar.user-info-navbar');
+
+		public_vars.wheelPropagation      = true; // used in Main menu (sidebar)
+
+		public_vars.defaultColorsPalette = ['#68b828','#7c38bc','#0e62c7','#fcd036','#4fcdfc','#00b19d','#ff6264','#f7aa47'];
+
+		// Setup Sidebar Menu
+		setup_sidebar_menu();
+
+		// Go to top links
+		$('body').on('click', 'a[rel="go-top"]', function(ev)
+		{
+			ev.preventDefault();
+
+			var obj = {pos: $(window).scrollTop()};
+
+			TweenLite.to(obj, .3, {pos: 0, ease:Power4.easeOut, onUpdate: function()
+			{
+				$(window).scrollTop(obj.pos);
+			}});
+		});
+
+		// User info navbar equal heights
+		if(public_vars.$userInfoMenu.length)
+		{
+			public_vars.$userInfoMenu.find('.user-info-menu > li').css({
+				minHeight: public_vars.$userInfoMenu.outerHeight() - 1
+			});
+		}
+	});
+})(jQuery, window);
+
+// Sideber Menu Setup function
+var sm_duration = .2,
+	sm_transition_delay = 150;
+
+function setup_sidebar_menu()
+{
+	if(public_vars.$sidebarMenu.length)
+	{
+		var $items_with_subs = public_vars.$sidebarMenu.find('li:has(> ul)'),
+			toggle_others = public_vars.$sidebarMenu.hasClass('toggle-others');
+
+		$items_with_subs.filter('.active').addClass('expanded');
+
+		// On larger screens collapse sidebar when the window is tablet screen
+		if(is('largescreen') && public_vars.$sidebarMenu.hasClass('collapsed') == false)
+		{
+			$(window).on('resize', function()
+			{
+				if(is('tabletscreen'))
+				{
+					public_vars.$sidebarMenu.addClass('collapsed');
+					ps_destroy();
+				}
+				else
+				if(is('largescreen'))
+				{
+					public_vars.$sidebarMenu.removeClass('collapsed');
+					ps_init();
+				}
+			});
+		}
+
+		$items_with_subs.each(function(i, el)
+		{
+			var $li = jQuery(el),
+				$a = $li.children('a'),
+				$sub = $li.children('ul');
+
+			$li.addClass('has-sub');
+
+			$a.on('click', function(ev)
+			{
+				ev.preventDefault();
+
+				if(toggle_others)
+				{
+					sidebar_menu_close_items_siblings($li);
+				}
+
+				if($li.hasClass('expanded') || $li.hasClass('opened'))
+					sidebar_menu_item_collapse($li, $sub);
+				else
+					sidebar_menu_item_expand($li, $sub);
+			});
+		});
+	}
+}
+
+function sidebar_menu_item_expand($li, $sub)
+{
+	if($li.data('is-busy') || ($li.parent('.main-menu').length && public_vars.$sidebarMenu.hasClass('collapsed')))
+		return;
+
+	$li.addClass('expanded').data('is-busy', true);
+	$sub.show();
+
+	var $sub_items 	  = $sub.children(),
+		sub_height	= $sub.outerHeight(),
+
+		win_y			 = jQuery(window).height(),
+		total_height	  = $li.outerHeight(),
+		current_y		 = public_vars.$sidebarMenu.scrollTop(),
+		item_max_y		= $li.position().top + current_y,
+		fit_to_viewpport  = public_vars.$sidebarMenu.hasClass('fit-in-viewport');
+
+	$sub_items.addClass('is-hidden');
+	$sub.height(0);
+
+
+	TweenMax.to($sub, sm_duration, {css: {height: sub_height}, onUpdate: ps_update, onComplete: function(){
+		$sub.height('');
+	}});
+
+	var interval_1 = $li.data('sub_i_1'),
+		interval_2 = $li.data('sub_i_2');
+
+	window.clearTimeout(interval_1);
+
+	interval_1 = setTimeout(function()
+	{
+		$sub_items.each(function(i, el)
+		{
+			var $sub_item = jQuery(el);
+
+			$sub_item.addClass('is-shown');
+		});
+
+		var finish_on = sm_transition_delay * $sub_items.length,
+			t_duration = parseFloat($sub_items.eq(0).css('transition-duration')),
+			t_delay = parseFloat($sub_items.last().css('transition-delay'));
+
+		if(t_duration && t_delay)
+		{
+			finish_on = (t_duration + t_delay) * 1000;
+		}
+
+		// In the end
+		window.clearTimeout(interval_2);
+
+		interval_2 = setTimeout(function()
+		{
+			$sub_items.removeClass('is-hidden is-shown');
+
+		}, finish_on);
+
+
+		$li.data('is-busy', false);
+
+	}, 0);
+
+	$li.data('sub_i_1', interval_1),
+	$li.data('sub_i_2', interval_2);
+}
+
+function sidebar_menu_item_collapse($li, $sub)
+{
+	if($li.data('is-busy'))
+		return;
+
+	var $sub_items = $sub.children();
+
+	$li.removeClass('expanded').data('is-busy', true);
+	$sub_items.addClass('hidden-item');
+
+	TweenMax.to($sub, sm_duration, {css: {height: 0}, onUpdate: ps_update, onComplete: function()
+	{
+		$li.data('is-busy', false).removeClass('opened');
+
+		$sub.attr('style', '').hide();
+		$sub_items.removeClass('hidden-item');
+
+		$li.find('li.expanded ul').attr('style', '').hide().parent().removeClass('expanded');
+
+		ps_update(true);
+	}});
+}
+
+function sidebar_menu_close_items_siblings($li)
+{
+	$li.siblings().not($li).filter('.expanded, .opened').each(function(i, el)
+	{
+		var $_li = jQuery(el),
+			$_sub = $_li.children('ul');
+
+		sidebar_menu_item_collapse($_li, $_sub);
+	});
+}
+
+// Perfect scroll bar functions by Arlind Nushi
+function ps_update(destroy_init)
+{
+	if(isxs())
+		return;
+
+	if(jQuery.isFunction(jQuery.fn.perfectScrollbar))
+	{
+		if(public_vars.$sidebarMenu.hasClass('collapsed'))
+		{
+			return;
+		}
+
+		public_vars.$sidebarMenu.find('.sidebar-menu-inner').perfectScrollbar('update');
+
+		if(destroy_init)
+		{
+			ps_destroy();
+			ps_init();
+		}
+	}
+}
+
+function ps_init()
+{
+	if(isxs())
+		return;
+
+	if(jQuery.isFunction(jQuery.fn.perfectScrollbar))
+	{
+		if(public_vars.$sidebarMenu.hasClass('collapsed') || ! public_vars.$sidebarMenu.hasClass('fixed'))
+		{
+			return;
+		}
+
+		public_vars.$sidebarMenu.find('.sidebar-menu-inner').perfectScrollbar({
+			wheelSpeed: 1,
+			wheelPropagation: public_vars.wheelPropagation
+		});
+	}
+}
+
+function ps_destroy()
+{
+	if(jQuery.isFunction(jQuery.fn.perfectScrollbar))
+	{
+		public_vars.$sidebarMenu.find('.sidebar-menu-inner').perfectScrollbar('destroy');
+	}
+}
+
 ;(function($, window, undefined)
 {
 	"use strict";
@@ -266,8 +524,8 @@ let title2 = `
 ==============================
 `
 let content = `
-版 本 号：4.0.4
-更新日期：2024-10-18
+版 本 号：4.1.0
+更新日期：2024-10-21
 
 WayneのNav: https://
 Github:  https://github.com/Waynenet/Wayne-Navigation
